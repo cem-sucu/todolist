fb121940626f4c8e1bbd03e165aadc8df06c9058
cem-sucu <cems.cems.17@gmail.com>
2023-02-08 13:05:58 +0100

    ajouter élément

---
+++ script.js
@@ -0,0 +1,21 @@
+var addToDoButton = document.getElementById("addToDo");
+var toDoContainer = document.getElementById("toDoContainer");
+var inputField = document.getElementById("inputField");
+
+addToDoButton.onclick = function () {
+    // verifier si l'input n'est pas vide
+    if (inputField.value != "") {
+        //si l'input n'est pas vide on créer le paragraphe
+        var paragraph = document.createElement("p");
+    }
+    //valorise le paragraphe avec le contenu de l'input
+    paragraph.innerHTML = inputField.value;
+
+    //style du paragaphe
+    paragraph.classList.add("paragraphe_style")
+    //insérer le paragraphe dans l'élément toDoContainer
+    toDoContainer.appendChild(paragraph);
+
+    //on vide l'input une fois le paragraphe ajouté
+    inputField.value = "";
+};
